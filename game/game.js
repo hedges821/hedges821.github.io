2b657a13b4171c1fe65083431f4ecac25924ad43
Mike Hedges <mike.hedges821@gmail.com>
2022-06-22 20:47:07 -0500

    finished game tutorial

--- game/game.js
+++ game/game.js
@@ -102,6 +102,13 @@ function create ()
 
     //check if player collides with stars. if so, pass to collectStar function
     this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    //bombs
+    bombs = this.physics.add.group();
+
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
     
 }
 
@@ -137,4 +144,33 @@ function collectStar (player, star)
 
     score += 10;
     scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}
+
+//turn the player red if they touch a bomb
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
